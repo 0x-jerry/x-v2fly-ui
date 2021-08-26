eceb785660b8d31e2454945efe8c0ae71d2818af
0x-jerry <x.jerry.wang@gmail.com>
2021-08-26 22:09:49 +0800

    feat: update layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const appSize = {
+  w: '800px',
+  h: '600px',
+}
+
+const conf = reactive({
+  port: 7999,
+  b64: '',
+})
+
+function connectToWebsocket() {
+  console.log('hello')
+}
+
+function stopService() {
+  //
+}
+
+function startService() {
+  //
+}
+</script>
 
 <template>
-  <TTheme class="h-screen">
-    <TContainer title="container">
-      <TButton>cool</TButton>
+  <TTheme class="app" w="min-content" m="auto">
+    <TContainer title="V2fly UI">
+      <div class="size">
+        <div flex="~" justify="end" items="center">
+          <span m="x-2">Port: </span>
+          <TInput placeholder="port" type="number" v-model="conf.port" m="x-2" />
+          <TButton @click="connectToWebsocket"> Connect </TButton>
+        </div>
+        <hr m="y-2" />
+        <div flex="~ col">
+          <TInput placeholder="VMess url" />
+          <TButton m="t-2" @click="startService"> Restart </TButton>
+          <TButton m="t-2" @click="stopService"> Stop </TButton>
+        </div>
+      </div>
     </TContainer>
   </TTheme>
 </template>
 
-<style></style>
+<style>
+html,
+body {
+  background: #f0f0f0;
+}
+
+.size {
+  max-width: v-bind('appSize.w');
+  max-height: v-bind('appSize.h');
+}
+</style>
