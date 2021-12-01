4e7ed0bf0f8ae7f82df69196c8444c871a12f996
0x-jerry <x.jerry.wang@gmail.com>
2021-12-01 20:31:35 +0800

    fix: bind this

--- src/v2ray.ts
+++ src/v2ray.ts
@@ -4,9 +4,9 @@ export const socket = new Socket('ws://127.0.0.1:7999')
 
 const client = new ProtocolClient()
 
-client.setSender(socket.send)
+client.setSender((data) => socket.send(data))
 
-socket.on('message', client.resolve)
+socket.on('message', (data) => client.resolve(data))
 
 export function start(b64: string) {
   client.send('start-v2fly', { b64 })
