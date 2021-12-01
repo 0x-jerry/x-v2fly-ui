02783f6df0a0a8b95236f60dc96f047b4763593a
0x-jerry <x.jerry.wang@gmail.com>
2021-12-01 16:06:16 +0800

    perf: upgrade deps

--- src/v2ray.ts
+++ src/v2ray.ts
@@ -1,25 +1,45 @@
-import { Socket, createProtocolMessage, Protocol } from '@0x-jerry/lib'
+import { Socket, ProtocolClient } from '@0x-jerry/lib'
 
 export const socket = new Socket('ws://127.0.0.1:7999')
 
-socket.on('message', (protocol: any) => {
-  socket.emit(protocol.id, protocol)
+const client = new ProtocolClient()
+
+client.setSender((data) => {
+  socket.send(data)
 })
 
+socket.on('message', (data: any) => {
+  client.resolve(data)
+})
+
+interface ProtocolData<Data = any, MsgType = string> {
+  id: string
+  type: MsgType
+  data?: Data
+}
+
+function createProtocolMessage(type: string, data?: any): ProtocolData {
+  return {
+    id: Math.random().toString(16).substr(2),
+    type,
+    data,
+  }
+}
+
 export function start(b64: string) {
-  socket.send(createProtocolMessage('start-v2fly', b64))
+  socket.send(createProtocolMessage('start-v2fly', { b64 }))
 }
 
 export function stop() {
-  socket.send(createProtocolMessage('stop-v2fly', {}))
+  socket.send(createProtocolMessage('stop-v2fly'))
 }
 
 export function getConf() {
   return new Promise((resolve) => {
-    const msg = createProtocolMessage('get-v2fly-conf', {})
+    const msg = createProtocolMessage('get-v2fly-conf')
 
-    socket.once(msg.id as any, (protocol: any) => {
-      resolve(protocol.data)
+    socket.once(msg.id as any, (data: any) => {
+      resolve(data)
     })
 
     socket.send(msg)
